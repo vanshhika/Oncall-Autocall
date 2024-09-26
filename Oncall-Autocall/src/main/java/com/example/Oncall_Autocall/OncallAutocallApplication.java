43a83f1c0e3cbe0c90bca378c52b0471f6c63fab
vanshhika <vinnypatel2000@gmail.com>
2024-09-26 13:56:50 -0700

    Initial commit

---
+++ Oncall-Autocall/src/main/java/com/example/Oncall_Autocall/OncallAutocallApplication.java
@@ -0,0 +1,13 @@
+package com.example.Oncall_Autocall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncallAutocallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OncallAutocallApplication.class, args);
+	}
+
+}
---
+++ Oncall-Autocall/src/test/java/com/example/Oncall_Autocall/OncallAutocallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Oncall_Autocall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OncallAutocallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
